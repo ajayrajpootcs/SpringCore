fef2dd084f5d6d24afeef657bf5532495455f934
Ajay Rajput <ajayrajpoot.cs@gmail.com>
2024-02-05 23:28:09 +0530

    AutoWiring Using ByName , ByType , contructor

--- src/main/java/spring/core/App.java
+++ src/main/java/spring/core/App.java
@@ -2,43 +2,17 @@ package spring.core;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import spring.model.DbSingleton;
 import spring.model.Employee;
 
 public class App {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args){
 		ApplicationContext IOC = new ClassPathXmlApplicationContext("ApplicationContext.xml");
-//		Employee e =IOC.getBean(Employee.class,"Kunal");
-		Employee e2 = (Employee)IOC.getBean("child");
-//		Employee e2 = IOC.getBean(Employee.class);
-//		System.out.println(e);
-		System.out.println(e2);
-		
-//		Bean LifeCycle
-//		Employee e3 = (Employee)IOC.getBean("emp2");
-//		System.err.println(e3);
-		
-		//Inner Bean
-//		Employee e4 = (Employee)IOC.getBean("emp3");
-//		System.err.println(e4);
-//		e4.getAddress().setCity("BLY");
-//		System.err.println(e4);
-//		
-//		Employee e5 = (Employee)IOC.getBean("emp3");
-//		System.err.println(e5);
-//		e4.getAddress().setCity("GZB");
-//		System.err.println(e4);
-//		
-		
-		//Factory Method to get Single bean OBJ
-//		DbSingleton e6 = (DbSingleton)IOC.getBean("singleton");
-//		System.err.println(e6);
-//		DbSingleton e7 = (DbSingleton)IOC.getBean("singleton");
-//		System.err.println(e7);
-		
+
+		Employee e = (Employee)IOC.getBean(Employee.class);
+		System.out.println(e);
 		
 		
 	}
 
+	
 }
