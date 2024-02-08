6e90903b8300662c3c004f6d3dcaf6f24c5d9bd1
Ajay Rajput <ajayrajpoot.cs@gmail.com>
2024-02-08 15:21:30 +0530

    setting-Up @Component Again

--- src/main/java/spring/core/App.java
+++ src/main/java/spring/core/App.java
@@ -4,15 +4,14 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import spring.model.Employee;
 
-
 public class App {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		ApplicationContext IOC = new ClassPathXmlApplicationContext("ApplicationContext.xml");
-		Employee e = (Employee)IOC.getBean(Employee.class);
-		System.out.println(e);
-		
-		
+		// Employee e = (Employee)IOC.getBean(Employee.class);
+		// System.out.println(e);
+		Employee bean = IOC.getBean("employee", Employee.class);
+		System.out.println(bean);
+
 	}
 
-	
 }
